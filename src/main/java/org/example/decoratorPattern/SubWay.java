1c6c58c9f7a0792b4b0eb7504d6095dbdf8cad43
kvsmanikanta <kvsmanikanta@gmail.com>
2024-04-03 12:48:36 +0530

    Decorator Pattern

---
+++ src/main/java/org/example/decoratorPattern/SubWay.java
@@ -0,0 +1,9 @@
+package org.example.decoratorPattern;
+
+public interface SubWay {
+    //we have methods for getting the price and Description of Pizza
+
+    public int getPrice();
+    public String getDescription();
+
+}
